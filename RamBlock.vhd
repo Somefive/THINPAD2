c96d10883c668ffcd62c111e59b3c3a78c74760d
kandorm <d-wan14@mails.tsinghua.edu.cn>
2016-11-17 18:59:13 +0800

    RamBlock

--- RamBlock.vhd
+++ RamBlock.vhd
@@ -37,7 +37,19 @@ entity RamBlock is
 			  RamControl : in STD_LOGIC_VECTOR(15 downto 0);
 			  Finish : out STD_LOGIC;
 			  Data : out STD_LOGIC_VECTOR(15 downto 0);
-			  Instruction : out STD_LOGIC_VECTOR(15 downto 0));
+			  Instruction : out STD_LOGIC_VECTOR(15 downto 0);
+			  ADDR : out  STD_LOGIC_VECTOR (17 downto 0);
+           DATA : inout  STD_LOGIC_VECTOR (15 downto 0);
+           EN : out  STD_LOGIC;
+           OE : out  STD_LOGIC;
+           WE : out  STD_LOGIC;
+			  DATA_READY : in  STD_LOGIC;
+           RDN : out  STD_LOGIC;
+           TBRE : in  STD_LOGIC;
+           TSRE : in  STD_LOGIC;
+           WRN : out  STD_LOGIC;
+           CLK : in  STD_LOGIC;
+           MODE : in  STD_LOGIC_VECTOR (1 downto 0)); --"00" Disabled; "01" Read; "10" Write; "11" Enabled;
 end RamBlock;
 
 architecture Behavioral of RamBlock is
