5641d36456b13e8702527ba63390815d58053b9b
Somefive <Somefive@foxmail.com>
2016-11-17 17:53:48 +0800

    init

---
+++ CPU.vhd
@@ -0,0 +1,79 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:45:02 11/17/2016 
+-- Design Name: 
+-- Module Name:    CPU - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity CPU is
+    Port ( SW_DIP : in  STD_LOGIC_VECTOR (15 downto 0);
+           RAM1DATA : inout  STD_LOGIC_VECTOR (15 downto 0);
+           RAM2DATA : inout  STD_LOGIC_VECTOR (15 downto 0);
+           RAM1ADDR : out  STD_LOGIC_VECTOR (17 downto 0);
+           RAM2ADDR : out  STD_LOGIC_VECTOR (17 downto 0);
+           RAM1_EN : out  STD_LOGIC;
+           RAM1_OE : out  STD_LOGIC;
+           RAM1_RW : out  STD_LOGIC;
+           RAM2_EN : out  STD_LOGIC;
+           RAM2_OE : out  STD_LOGIC;
+           RAM2_RW : out  STD_LOGIC;
+           FPGA_LED : out  STD_LOGIC_VECTOR (15 downto 0);
+			  DYP0 : out  STD_LOGIC_VECTOR (6 downto 0);
+           DYP1 : out  STD_LOGIC_VECTOR (6 downto 0);
+			  DATA_READY : in  STD_LOGIC;
+           RDN : out  STD_LOGIC;
+           TBRE : in  STD_LOGIC;
+           TSRE : in  STD_LOGIC;
+           WRN : out  STD_LOGIC;
+           CLK : in  STD_LOGIC;
+           RESET : in  STD_LOGIC);
+end CPU;
+
+architecture Behavioral of CPU is
+
+
+
+begin
+
+	RAM1DATA <= "0000000000000000";
+	RAM2DATA <= "0000000000000000";
+	RAM1ADDR <= "000000000000000000";
+	RAM2ADDR <= "000000000000000000";
+   RAM1_EN <= '1';
+	RAM1_OE <= '1';
+	RAM1_RW <= '1';
+	RAM2_EN <= '1';
+	RAM2_OE <= '1';
+	RAM2_RW <= '1';
+	FPGA_LED <= "0000000000000000";
+	DYP0 <= "0000000";
+	DYP1 <= "0000000";
+	RDN <= '1';
+	WRN <= '1';
+
+end Behavioral;
+
---
+++ Main.vhd
@@ -0,0 +1,109 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:35:31 11/17/2016 
+-- Design Name: 
+-- Module Name:    Main - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Main is
+    Port ( SW_DIP : in  STD_LOGIC_VECTOR (15 downto 0);
+           CLK1 : in  STD_LOGIC;
+           RAM1DATA : inout  STD_LOGIC_VECTOR (15 downto 0);
+           RAM2DATA : inout  STD_LOGIC_VECTOR (15 downto 0);
+           RAM1ADDR : out  STD_LOGIC_VECTOR (17 downto 0);
+           RAM2ADDR : out  STD_LOGIC_VECTOR (17 downto 0);
+           RAM1_EN : out  STD_LOGIC;
+           RAM1_OE : out  STD_LOGIC;
+           RAM1_RW : out  STD_LOGIC;
+           RAM2_EN : out  STD_LOGIC;
+           RAM2_OE : out  STD_LOGIC;
+           RAM2_RW : out  STD_LOGIC;
+           FPGA_LED : out  STD_LOGIC_VECTOR (15 downto 0);
+           DYP0 : out  STD_LOGIC_VECTOR (6 downto 0);
+           DYP1 : out  STD_LOGIC_VECTOR (6 downto 0);
+			  DATA_READY : in  STD_LOGIC;
+           RDN : out  STD_LOGIC;
+           TBRE : in  STD_LOGIC;
+           TSRE : in  STD_LOGIC;
+           WRN : out  STD_LOGIC;
+           CLK_FROM_KEY : in  STD_LOGIC;
+           RESET : in  STD_LOGIC);
+end Main;
+
+architecture Behavioral of Main is
+
+component CPU is
+    Port ( SW_DIP : in  STD_LOGIC_VECTOR (15 downto 0);
+           RAM1DATA : inout  STD_LOGIC_VECTOR (15 downto 0);
+           RAM2DATA : inout  STD_LOGIC_VECTOR (15 downto 0);
+           RAM1ADDR : out  STD_LOGIC_VECTOR (17 downto 0);
+           RAM2ADDR : out  STD_LOGIC_VECTOR (17 downto 0);
+           RAM1_EN : out  STD_LOGIC;
+           RAM1_OE : out  STD_LOGIC;
+           RAM1_RW : out  STD_LOGIC;
+           RAM2_EN : out  STD_LOGIC;
+           RAM2_OE : out  STD_LOGIC;
+           RAM2_RW : out  STD_LOGIC;
+           FPGA_LED : out  STD_LOGIC_VECTOR (15 downto 0);
+			  DYP0 : out  STD_LOGIC_VECTOR (6 downto 0);
+           DYP1 : out  STD_LOGIC_VECTOR (6 downto 0);
+			  DATA_READY : in  STD_LOGIC;
+           RDN : out  STD_LOGIC;
+           TBRE : in  STD_LOGIC;
+           TSRE : in  STD_LOGIC;
+           WRN : out  STD_LOGIC;
+           CLK : in  STD_LOGIC;
+           RESET : in  STD_LOGIC);
+end component;
+
+begin
+
+	CPU_ENTITY: CPU port map ( 
+		SW_DIP,
+		RAM1DATA,
+		RAM2DATA,
+		RAM1ADDR,
+		RAM2ADDR,
+		RAM1_EN,
+		RAM1_OE,
+		RAM1_RW,
+		RAM2_EN,
+		RAM2_OE,
+		RAM2_RW,
+		FPGA_LED,
+		DYP0,
+		DYP1,
+		DATA_READY,
+		RDN,
+		TBRE,
+		TSRE,
+		WRN,
+		CLK_FROM_KEY,
+		RESET);
+
+end Behavioral;
+
